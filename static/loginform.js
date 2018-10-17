08e06d603937a8a575e6a998a1eb38ed37858cc9
ihardy <ihardy@wrlc.org>
2018-10-17 14:27:37 -0400

    show logged in name, add gt, remove old js

--- static/loginform.js
+++
@@ -1,9 +0,0 @@
-// login urls
-function submitLogin() {
-    var sp = "https://aladin-sp.wrlc.org/simplesaml/wrlcauth/issue.php?institution="
-    var params = "&url=https://fines.wrlc.org/login/n"
-    var select = document.getElementById('user-name');
-    var institution = select.options[select.selectedIndex].value;
-    window.location.replace(sp + institution + params);
-    return false;
-}
